057df7fafa619d4ac3edc431a04167afc9dba7b7
Chamsmey <malwareattacker2022@gmail.com>
2021-12-29 00:52:39 +0700

    reduce code

--- game/game.py
+++ game/game.py
@@ -9,20 +9,18 @@ canvas=tk.Canvas(frame,width=100,height=100)
 bg=tk.PhotoImage(file="image\grounde.png")
 bg2=tk.PhotoImage(file="image\Pro.png")
 canvas=tk.Canvas(frame,width=500,height=550)
-def Disply():
-    canvas.create_image(0,0,image=bg2,anchor="nw")
-Disply()
+
 def play():
+    global isPlay 
+    isPlay=True
+    displayGrid()
     global buttonPlay
     moveFruit()
-    buttonPlay.pack_forget()
-
-    buttonPlay=tk.Button(canvas,text="Pause",bg="#64ffda",bd=1,fg="white",font=("",15),padx=10,pady=0,width=5,height=0,command=play)
-    buttonPlay.pack(side="bottom")
-
+    
 ##button----------------------------------------------------
 
 ##global variable------------------
+isPlay=False
 score=0
 lives=5
 grid=[
@@ -38,66 +36,61 @@ grid=[
 ##disply grid----------------------------------------------------------------------------------
 def displayGrid():
     canvas.delete("all")
-    global grid,positionfruit,  score ,lives ,fruits
-    ##-bg----------------------------------------
-    canvas.create_image(0,0,image=bg,anchor="nw")
-    ##--score--------------------------------------
-    text=canvas.create_text(40,50,text=" Score:"+str(score),font=("",15))
-    
-
-    ##grid--------------------
-    x1=100
-    x2=150
-    y1=100
-    y2=150
-    for row in grid:
-        for col in row:
-            if col==1:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=robot)
-            # elif col==2:
-            elif col==4:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[0])
-            elif col==5:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[1])
-            elif col==6:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[2])
-            elif col==7:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[3])
-            elif col==8:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[4])
-            elif col==9:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[5])
-            elif col==10:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[6])
-            elif col==11:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=fruits[7])
-            elif col==3:
-                canvas.create_image(x2-25,y2-25,image=wall)
-                canvas.create_image(x2-25,y2-25,image=bomb)
-            else:
-                canvas.create_image(x2-25,y2-25,image=wall)
-            x1=x2
-            x2+=50  
-        y1=y2
-        y2+=50
+    global grid,positionfruit,  score ,lives ,isPlay
+    ##-bg-------------------------------------------------------------------------------------------
+    if not isPlay:
+        canvas.create_image(0,0,image=bg2,anchor="nw")
+        canvas.create_text(350,100,text="Welcome to the best eater",font=("",30),fill="white")
+        buttonPlay=tk.Button(canvas,text="Play",bg="#64ffda",bd=1,fg="white",font=("",15),padx=10,pady=0,width=5,height=0,command=play)
+        buttonExit=tk.Button(canvas,text="Exit",bg="#64ffda",bd=1,fg="white",font=("",15),padx=10,pady=0,width=5,height=0,command=root.destroy)
+        buttonExit.pack(side="bottom")
+        buttonPlay.pack(side="bottom")
+    else:
+        canvas.create_image(0,0,image=bg,anchor="nw")
+        ##--score--------------------------------------
+        text=canvas.create_text(40,50,text=" Score:"+str(score),font=("",15))
+        
+        ##grid--------------------
         x1=100
         x2=150
+        y1=100
+        y2=150
+        for row in grid:
+            for col in row:
+                canvas.create_image(x2-25,y2-25,image=wall)
+                if col==1:
+                    canvas.create_image(x2-25,y2-25,image=robot)
+                elif col==4:
+                    canvas.create_image(x2-25,y2-25,image=fruits[0])
+                elif col==5:
+                    canvas.create_image(x2-25,y2-25,image=fruits[1])
+                elif col==6:
+                    canvas.create_image(x2-25,y2-25,image=fruits[2])
+                elif col==7:
+                    canvas.create_image(x2-25,y2-25,image=fruits[3])
+                elif col==8:
+                    canvas.create_image(x2-25,y2-25,image=fruits[4])
+                elif col==9:
+                    canvas.create_image(x2-25,y2-25,image=fruits[5])
+                elif col==10:
+                    canvas.create_image(x2-25,y2-25,image=fruits[6])
+                elif col==11:
+                    canvas.create_image(x2-25,y2-25,image=fruits[7])
+                elif col==3:
+                    canvas.create_image(x2-25,y2-25,image=bomb)
+                x1=x2
+                x2+=50  
+            y1=y2
+            y2+=50
+            x1=100
+            x2=150
 
-    ## lives -------------------
-    x=20
-    y=20
-    for i in range(lives):
-        canvas.create_image(x,y,image=heart)
-        x+=30
+        ## lives -------------------
+        x=20
+        y=20
+        for i in range(lives):
+            canvas.create_image(x,y,image=heart)
+            x+=30
     ##----------Button--------------------------
     
 ##rendom----------------------------------------
@@ -111,8 +104,8 @@ def ramdomFruit():
     displayGrid()
 ##move fruit ==------------------------------------
 def moveFruit():
-    global grid,score ,lives
-
+    global grid,score ,lives, isPlay
+    
     Fruitgrid=[
         [0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0],
@@ -153,7 +146,6 @@ def moveFruit():
                 elif (grid[row][col]==4) and (grid[row+1][col]!=1):
                     grid[row][col]=0
                     Fruitgrid[row+1][col]=4
-                    Fruitgrid[row+1][col]=4
                 elif (grid[row][col]==5) and (grid[row+1][col]!=1):
                     Fruitgrid[row+1][col]=5
                 elif (grid[row][col]==6) and (grid[row+1][col]!=1):
@@ -170,7 +162,7 @@ def moveFruit():
                     Fruitgrid[row+1][col]=11
             elif( grid[row][col]==1):
                 Fruitgrid[row][col]=1
-    grid=Fruitgrid    
+    grid=Fruitgrid   
     displayGrid()
     canvas.after(time,lambda:moveFruit()) 
     canvas.after(5000,lambda:ramdomFruit())    
@@ -204,14 +196,12 @@ def moveLeft(event):
     moveChar(-1)
 def moveRight(event):
     moveChar(1)
-##call function-----------
-buttonPlay=tk.Button(canvas,text="Play",bg="#64ffda",bd=1,fg="white",font=("",15),padx=10,pady=0,width=5,height=0,command=play)
-
-buttonExit=tk.Button(canvas,text="Exit",bg="#64ffda",bd=1,fg="white",font=("",15),padx=10,pady=0,width=5,height=0,command=root.destroy)
+##call function------------------------------------------
+displayGrid()
 ##Event-------------------------------
 root.bind("<Left>",moveLeft)
 root.bind("<Right>",moveRight)
-##---------------------------------
+##------------------------------------
 wall=tk.PhotoImage(file="image\wall.png")
 heart=tk.PhotoImage(file="image\live.png")
 fruit1=tk.PhotoImage(file="icons\lemon.png")
@@ -228,7 +218,5 @@ bomb=tk.PhotoImage(file="icons\omb.png")
 char=tk.PhotoImage(file="icons\cutt.png")
 robot=tk.PhotoImage(file="image\jing.png")
 canvas.pack(expand=True,fill="both")
-buttonExit.pack(side="bottom")
-buttonPlay.pack(side="bottom")
 frame.pack(expand=True,fill="both")
 root.mainloop()
